23a9315087a83965848742c8816f05e681f410cc
yokekokevin <kevin.kennedy@comcast.net>
2026-06-24 23:49:28 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'yokeko-v1';
+const SHELL = ['/', '/index.html', '/manifest.json', '/icon-192.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.hostname === 'api.tidesandcurrents.noaa.gov') {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
